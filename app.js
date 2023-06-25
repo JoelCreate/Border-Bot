24ee81fd243e3faf371f996baf5fa32401322225
Joel Wiesner <j.wiesner88@gmail.com>
2023-06-25 13:34:44 -0700

    styled activity name in CSS

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ activityBtn.addEventListener("click", function() {
     .then(response => response.json())
     .then(data => {        
         activity.textContent = data.activity
-        activityBtn.textContent = "Give me another idea please!"        
+        activityBtn.classList.add('animate__animated', 'animate__bounce')
+        activityBtn.textContent = "Give me another idea please!"     
+        activityBtn.classList.remove()    
     })
 })
